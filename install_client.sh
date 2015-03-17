0d12b9bd7c6ca2e0adfd7adb7a5e020729c97e10
Corey Thuen <corey.thuen@southforksecurity.com>
2015-03-17 12:04:37 -0600

    correct beaglebone can interface and config file path

--- install_client.sh
+++ install_client.sh
@@ -4,5 +4,5 @@ cp ./linux-configs/client.conf /etc/init/canbusprotector.conf
 cp ./linux-configs/client.iptables.rules /etc/iptables.rules
 cp ./linux-configs/client.network.interfaces /etc/network/interfaces
 
-cp ./linux-configs/iptablesrestore.sh /etc/network/if-pre-up/d/iptables
+cp ./linux-configs/iptablesrestore.sh /etc/network/if-pre-up.d/iptables
 chmod +x /etc/network/if-pre-up.d/iptables
--- install_server.sh
+++ install_server.sh
@@ -4,5 +4,5 @@ cp ./linux-configs/server.conf /etc/init/canbusprotector.conf
 cp ./linux-configs/server.iptables.rules /etc/iptables.rules
 cp ./linux-configs/server.network.interfaces /etc/network/interfaces
 
-cp ./linux-configs/iptablesrestore.sh /etc/network/if-pre-up/d/iptables
+cp ./linux-configs/iptablesrestore.sh /etc/network/if-pre-up.d/iptables
 chmod +x /etc/network/if-pre-up.d/iptables
